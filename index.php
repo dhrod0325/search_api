56bfece50d50e86e6a6f84ce7fa85e96d2eec50d
dhrod0325 <dhrod0325@naver.com>
2014-11-04 10:14:36 +0900

    commit

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ function autoLoad( $className ) {
 spl_autoload_register( 'autoLoad' );
 
 $twitter = new \classes\api\search\impl\TwitterSearchApi( array(
-	'oauth_access_token'        => "2841900320-2V9jbrIptnPpQyYuOVprcOe3UPGgft6iqvqKB8F",
-	'oauth_access_token_secret' => "glwOqLHNCpNZFKCCRgKnstG64KF8z7IvD9B2C77UoRFuq",
-	'consumer_key'              => "DaWfqIc44zDP0pflMo9RgeCMv",
-	'consumer_secret'           => "CSqQCNDdVXXOo4stRkF4VL4yYCPpJAo5lRrBqsBFBuG05Yh5lU"
+	'oauth_access_token'        => "",
+	'oauth_access_token_secret' => "",
+	'consumer_key'              => "",
+	'consumer_secret'           => ""
 ) );
 
-$naver  = new \classes\api\search\impl\NaverSearchApi( array( 'api_key' => 'c1b406b32dbbbbeee5f2a36ddc14067f ' ) );
-$daum   = new \classes\api\search\impl\DaumSearchApi( array( 'api_key' => '6d85d2a3b6720acc1156c4b65cf04eb11db16037' ) );
+$naver  = new \classes\api\search\impl\NaverSearchApi( array( 'api_key' => ' ' ) );
+$daum   = new \classes\api\search\impl\DaumSearchApi( array( 'api_key' => '' ) );
 $google = new \classes\api\search\impl\GoogleSearchApi();
 
 $service = new \classes\api\search\service\SearchService();
