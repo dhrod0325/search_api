5c9e8094037c72112f0f99880400f2b5bc3ae8d8
dhrod0325 <dhrod0325@naver.com>
2014-11-06 09:24:13 +0900

    commit

--- README.md
+++ README.md
@@ -11,33 +11,24 @@ This is sns search api library
 ##How to use
 
 ```
-$twitter = new TwitterSearchApi(array(
-	'oauth_access_token' => "",
+require_once 'loader.php';
+
+$twitter  = new TwitterSearchApi( array(
+	'oauth_access_token'        => "",
 	'oauth_access_token_secret' => "",
-	'consumer_key' => "",
-	'consumer_secret' => ""
-));
-
-$naver = new NaverSearchApi(
-	array('api_key' => ' ')
-);
-
-$daum = new DaumSearchApi(
-	array('api_key' => '')
-);
-
-$google = new GoogleSearchApi();
-
-$facebook = new FacebookSearchApi(
-	array('app_id' => '',
-	      'app_secret' => ''
-	)
-);
-
-$service = new SearchService();
-$service->setSearchApi($facebook);
-
-print_r($service->getData('Test'));
+	'consumer_key'              => "",
+	'consumer_secret'           => ""
+) );
+$naver    = new NaverSearchApi( array( 'api_key' => ' ' ) );
+$daum     = new DaumSearchApi( array( 'api_key' => '' ) );
+$google   = new GoogleSearchApi();
+$facebook = new FacebookSearchApi( array(
+	'app_id'     => '',
+	'app_secret' => ''
+) );
+$service  = new SearchService();
+$service->setSearchApi( $facebook );
+print_r( $service->getData( 'Test' ) );
 ```
 
 [Make by Saweb] (http://www.saweb.co.kr)
--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-require_once 'loader.php';
-
 use classes\api\search\impl\DaumSearchApi;
 use classes\api\search\impl\FacebookSearchApi;
 use classes\api\search\impl\GoogleSearchApi;
@@ -8,30 +6,21 @@ use classes\api\search\impl\NaverSearchApi;
 use classes\api\search\impl\TwitterSearchApi;
 use classes\api\search\service\SearchService;
 
-$twitter = new TwitterSearchApi(array(
-	'oauth_access_token' => "",
+require_once 'loader.php';
+
+$twitter  = new TwitterSearchApi( array(
+	'oauth_access_token'        => "",
 	'oauth_access_token_secret' => "",
-	'consumer_key' => "",
-	'consumer_secret' => ""
-));
-
-$naver = new NaverSearchApi(
-	array('api_key' => ' ')
-);
-
-$daum = new DaumSearchApi(
-	array('api_key' => '')
-);
-
-$google = new GoogleSearchApi();
-
-$facebook = new FacebookSearchApi(
-	array('app_id' => '',
-	      'app_secret' => ''
-	)
-);
-
-$service = new SearchService();
-$service->setSearchApi($facebook);
-
-print_r($service->getData('Test'));
+	'consumer_key'              => "",
+	'consumer_secret'           => ""
+) );
+$naver    = new NaverSearchApi( array( 'api_key' => ' ' ) );
+$daum     = new DaumSearchApi( array( 'api_key' => '' ) );
+$google   = new GoogleSearchApi();
+$facebook = new FacebookSearchApi( array(
+	'app_id'     => '',
+	'app_secret' => ''
+) );
+$service  = new SearchService();
+$service->setSearchApi( $facebook );
+print_r( $service->getData( 'Test' ) );
